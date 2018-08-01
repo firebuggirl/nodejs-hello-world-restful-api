f7e2f2422dd9fb2ac1a63d59aa7b31b7a0db20fd
Juliette <julietteredheadgirl@yahoo.com>
2018-08-01 12:56:22 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,90 @@
+// Dependencies
+const http = require('http');
+var url = require('url');
+const handlers = require('./lib/handlers');
+const StringDecoder = require('string_decoder').StringDecoder;//Decode a stream of binary data (a buffer object) into a string:
+const config = require('./lib/config');
+ // Configure the server to respond to all requests with a string
+const server = http.createServer(function(req,res){
+
+  // Parse the url
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+  // Get the HTTP method
+  const method = req.method.toLowerCase();
+
+  //Get the headers as an object
+  const headers = req.headers;
+
+  // Get the payload,if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', function(data) {
+      buffer += decoder.write(data);
+  });
+
+  req.on('end', function() {
+      buffer += decoder.end();
+
+      // Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
+      const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+
+      // Construct the data object to send to the handler
+      let data = {
+        'trimmedPath' : trimmedPath,
+        'queryStringObject' : queryStringObject,
+        'method' : method,
+        'headers' : headers,
+        'payload' : buffer
+      };
+
+
+      // Route the request to the handler specified in the router
+      chosenHandler(data,function(statusCode,payload){
+
+        // Use the status code returned from the handler, or set the default status code to 200
+        statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+        // Use the payload returned from the handler, or set the default payload to an empty object
+        payload = typeof(payload) == 'object'? payload : {};
+
+        // Convert the payload to a string
+        const payloadString = JSON.stringify(payload);
+
+        // Return the response
+        res.writeHead(statusCode);
+        res.setHeader('Content-Type', 'application/json');
+        res.end(payloadString);
+        console.log("Returning this response: ",statusCode,payloadString);
+
+      });
+      // Send the response
+      res.end('Hello! This is a simple Node.js Restful API.\n');
+
+      // Log the request/response
+      console.log('Request received with this payload: ',buffer);
+  });
+
+
+
+  // Log the request/response
+//  console.log('Request received on path: '+trimmedPath+' with method: '+method+' and this query string: ',queryStringObject);
+});
+
+
+
+server.listen(config.port,function(){
+  console.log('The server is up and running on port '+config.port+' in '+config.envName+' mode.');
+});
+
+// Define the request router
+const router = {
+  'hello' : handlers.hello
+};
---
+++ readme.md
@@ -0,0 +1,19 @@
+## Homework # 1 The Node.js Master Class
+
+
+
+### Task: create a simple "Hello World" API
+
+1) Return a simple welcome message to Postman when a user posts anything to the `hello` route
+
+
+2)  Return the payload via the console
+
+
+- Test Restful API with Postman via `localhost:7777/hello` route via `POST` request with sample data in body to see :
+
+- send:
+
+ `  {
+    	"postmanMessage": "Hello world!"
+    } `
